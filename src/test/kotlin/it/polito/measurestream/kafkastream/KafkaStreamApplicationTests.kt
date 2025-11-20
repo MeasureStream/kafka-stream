81ecd4fcdf394c9b92bd3468fb66bd4922df5ff5
R3M1d4Cod3r <superchristian9@gmail.com>
2025-11-20 15:19:50 +0100

    fix: package name inside test folder

--- src/test/kotlin/it/polito/measurestream/kafkastream/KafkaStreamApplicationTests.kt
+++ src/test/kotlin/it/polito/measurestream/kafkastream/KafkaStreamApplicationTests.kt
@@ -1,13 +1,11 @@
-package it.polito.measurestream.kafka_stream
+package it.polito.measurestream.kafkastream
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class KafkaStreamApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
