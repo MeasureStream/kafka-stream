a0d414b28071d8d9f1c4972ecd73ea0514311883
sentomarco <sento.marco97@gmail.com>
2026-07-05 21:26:30 +0200

    FEAT: Added the extraction of the missing parameter FCnt (f_cnt) from TTN

--- src/main/kotlin/it/polito/measurestream/kafkastream/dto/TTNMessage.kt
+++ src/main/kotlin/it/polito/measurestream/kafkastream/dto/TTNMessage.kt
@@ -10,5 +10,10 @@ data class TTNMessage(
     val spreadingFactor: Int,
     val bandwidth: Long,
     val dataRate: String,
-    val consumedAirtime: String
+    val consumedAirtime: String,
+    /**
+     * Frame counter LoRaWAN dell'uplink (uplink_message.f_cnt nel JSON TTN).
+     * TTN omette il campo quando vale 0, quindi il default è 0.
+     */
+    val fCnt: Int = 0
 )
